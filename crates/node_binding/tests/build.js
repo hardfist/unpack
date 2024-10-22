d027afaabf674ab25729a002ec43be1d382e037c
yangjian.fe <yangjian.fe@bytedance.com>
2024-10-22 11:37:19 +1100

    chore: add release-debug

--- crates/node_binding/tests/build.js
+++ crates/node_binding/tests/build.js
@@ -2,9 +2,4 @@ const binding = require('..');
 const path = require('path');
 const context = path.resolve(__dirname, './fixtures');
 console.log('context:', context);
-binding.build(context, './src/index.mjs', [{
-    onResolve(err, path) {
-        
-        return undefined;
-    }
-}])
+binding.build(context, './src/index.mjs', [])
