ceffd06e2e67ed97036efe25fb6d810b02adfb0f
hardfist <yangjianzju@gmail.com>
2025-09-22 16:57:25 +0800

    chore: add tmp

--- crates/unpack_ng/src/db/module.rs
+++ crates/unpack_ng/src/db/module.rs
@@ -2,19 +2,11 @@
 use std::path::PathBuf;
 
 use swc_core::atoms::Atom;
-
-use crate::db::{ast::collector::ModuleReference, Db};
+use crate::db::Db;
+use crate::db::{ast::collector::ModuleReference};
 
 #[salsa::tracked]
 pub struct ESMModule<'db> {
     id: PathBuf,
     module_references: Vec<ModuleReference<'db>>,
-}
-
-#[salsa::tracked]
-impl<'db> ESMModule<'db> {
-    pub fn resolve_reference(&self, db: &'db dyn Db, name: Atom) -> Option<ESMModule> {
-        // db.lookup_module(name)
-        None
-    }
 }
