5c78c0375a5b26883ec320b68bc6fde0089c5228
hardfist <yangjianzju@gmail.com>
2025-10-05 09:47:03 +0800

    chore: add memory manager

--- crates/unpack_core/src/memory_manager/mod.rs
+++ crates/unpack_core/src/memory_manager/mod.rs
@@ -1,1 +1,19 @@
+use crate::{memory_manager::arena::{Arena, Idx}, module::BoxModule};
+
 pub mod arena;
+
+#[derive(Default,Debug)]
+pub struct MemoryManager {
+    pub modules: Arena<BoxModule>
+}
+
+impl MemoryManager {
+    pub fn new() -> Self {
+        Self {
+            modules: Default::default(),
+        }
+    }
+    pub fn alloc_module(&mut self, module: BoxModule) -> Idx<BoxModule> {
+        self.modules.insert(module)
+    }
+}
